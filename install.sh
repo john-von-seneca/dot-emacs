fd7165aab7a32967a5d76546459260ac9b0dbcf0
balasanjeevi <balasanjeevi.b@gmail.com>
2015-04-19 18:58:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/zsh
 
-if [[ ! -e ~/.cask ]]
-then
+if [[ ! -e ~/.cask ]]; then
     echo "Cloning Cask repo"
     git clone git@github.com:cask/cask.git ~/.cask
+else
+    echo "Cask already present"
 fi
 
-if [[ $(grep "cask/bin" ~/.zshrc) == "" ]]
-then
+if [[ $(grep "cask/bin" ~/.zshrc) == "" ]]; then
     echo "Adding \$HOME/.cask/bin to \$PATH in ~/.zshrc"
     echo '' >> ~/.zshrc
     echo "# Added by ~/.emacs.d/install.sh" >> ~/.zshrc
     echo "export PATH=\$HOME/.cask/bin:\$PATH" >> ~/.zshrc
+else
+    echo "Not updating ~/.zshrc"
 fi
 
 export PATH=$HOME/.cask/bin:$PATH
@@ -19,10 +21,12 @@ export PATH=$HOME/.cask/bin:$PATH
 cd ~/.emacs.d
 cask install
 
-if [[ ! -e ~/git-repos/prelude ]]
-then
+if [[ ! -e ~/git-repos/prelude ]]; then
     echo "Cloning prelude to ~/git-repos/prelude"
     git clone git@github.com:bbatsov/prelude.git ~/git-repos/prelude
+else
+    echo "Prelude already present"
+    
 fi
 
 # For Python / ELPY
@@ -37,7 +41,7 @@ fi
 EMACSINIT="$HOME/.emacs"
 if [ -L "$EMACSINIT" ]; then
   echo "$EMACSINIT found... backing up to ~/.emacs.d/.emacs.dot-emacs.bkp"
-  cp $EMACSINIT ~/.emacs.d/.emacs.dot.emacs.bkp
+  mv $EMACSINIT ~/.emacs.d/.emacs.dot.emacs.bkp
   ln -s $HOME/.emacs.d/.emacs $EMACSINIT
 else
   echo "creating symlink $EMACSINIT ... "
