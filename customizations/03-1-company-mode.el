072368d554228791d560e84628eb878166de87d7
Neo Human <balasanjeevi.b@gmail.com>
2015-06-08 04:08:04 +0530

    c++ ide

--- customizations/03-1-company-mode.el
+++ customizations/03-1-company-mode.el
@@ -3,8 +3,9 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
 (setq company-backends (delete 'company-semantic company-backends))
+(require 'cc-mode)
 (define-key c-mode-map  [(tab)] 'company-complete)
 (define-key c++-mode-map  [(tab)] 'company-complete)
 
 (add-to-list 'company-backends 'company-c-headers)
-(add-to-list 'company-c-headers-path-system "/usr/include/c++/4.8/")
+;; (add-to-list 'company-c-headers-path-system "/usr/include/c++/4.8/")
